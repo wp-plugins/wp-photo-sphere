c8539bcbf8f4167d33be08cf42cdc705ee168f3f
Jeremy Heleine <Jeremy Heleine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-01 07:13:28 +0000

    Version 3.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-photo-sphere/tags/3.1@1150810 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-photo-sphere.php
+++ wp-photo-sphere.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * WP Photo Sphere v2.3.1
+ * WP Photo Sphere v3.1
  * http://jeremyheleine.me/#wp-photo-sphere
  *
- * Copyright (c) 2013,2014 Jérémy Heleine
+ * Copyright (c) 2013-2015 Jérémy Heleine
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,31 +27,51 @@
 /*
 Plugin Name: WP Photo Sphere
 Plugin URI: http://jeremyheleine.me/#wp-photo-sphere
-Description: A filter that displays 360-degree panoramas taken with Photo Sphere. Please read the readme file for instructions.
-Version: 2.3.1
+Description: A filter that displays 360×180 degree panoramas. Please read the readme file for instructions.
+Version: 3.1
 Author: Jérémy Heleine
 Author URI: http://jeremyheleine.me
+Text Domain: wp-photo-sphere
+Domain Path: /lang/
 License: MIT
 */
 
 function wpps_activation() {
-	update_option('wpps_settings', array('style' => 'margin: 10px auto;', 'style_a' => 'padding: 5px; background-color: #3D3D3D; color: #FFFFFF;', 'class_a' => '', 'text' => 'WP Photo Sphere (%title%)', 'width' => '560px', 'max_width' => '100%', 'height' => '315px', 'hide_link' => 0));
+	update_option('wpps_settings', array(
+			'style' => 'margin: 10px auto;',
+			'style_a' => 'padding: 5px; background-color: #3D3D3D; color: #FFFFFF;',
+			'class_a' => '',
+			'text' => 'WP Photo Sphere (%title%)',
+			'width' => '560px',
+			'max_width' => '100%',
+			'height' => '315px',
+			'hide_link' => 0,
+			'anim_speed' => '2rpm',
+			'navbar' => 0,
+			'min_fov' => 30,
+			'max_fov' => 90,
+			'xmp' => 1
+		));
 }
+register_activation_hook(__FILE__, 'wpps_activation');
 
 function wpps_deactivation() {
 	delete_option('wpps_settings');
 }
+register_deactivation_hook(__FILE__, 'wpps_deactivation');
 
-function wpps_enqueue_scripts() {
-	wp_enqueue_script('wpps-three', plugin_dir_url(__FILE__) . 'lib/three.min.js', array(), '1.0', true);
-	wp_enqueue_script('wpps-psv', plugin_dir_url(__FILE__) . 'lib/photo-sphere-viewer.js', array(), '1.0', true);
-	wp_enqueue_script('wp-photo-sphere', plugin_dir_url(__FILE__) . 'wp-photo-sphere.js', array('jquery'), '2.1', true);
+function wpps_register_scripts() {
+	wp_register_script('wpps-three', plugin_dir_url(__FILE__) . 'lib/three.min.js', array(), '1.0', true);
+	wp_register_script('wpps-psv', plugin_dir_url(__FILE__) . 'lib/photo-sphere-viewer.js', array('wpps-three'), '2.0.1', true);
+	wp_register_script('wp-photo-sphere', plugin_dir_url(__FILE__) . 'wp-photo-sphere.js', array('jquery', 'wpps-psv'), '2.1', true);
 }
+add_action('plugins_loaded', 'wpps_register_scripts');
 
 function wpps_enqueue_admin_scripts() {
 	if (floatval(get_bloginfo('version')) >= 3.5)
 		wp_enqueue_script('wpps-admin', plugin_dir_url(__FILE__) . 'wpps-admin.js', array('jquery'), '1.0', true);
 }
+add_action('wp_enqueue_media', 'wpps_enqueue_admin_scripts');
 
 function wpps_add_pano_button() {
 	if (floatval(get_bloginfo('version')) >= 3.5) {
@@ -63,39 +83,129 @@ function wpps_add_pano_button() {
 		<?php
 	}
 }
+add_action('media_buttons', 'wpps_add_pano_button', 15);
 
 function wpps_lang() {
 	// i18n
 	load_plugin_textdomain('wp-photo-sphere', false, dirname(plugin_basename(__FILE__)) . '/lang');
 }
+add_action('plugins_loaded', 'wpps_lang');
+
+function wpps_shortcode_attributes($atts) {
+	if (!empty($atts)) {
+		$sizes = array('width', 'max_width');
+		$numbers = array('height', 'anim_after');
+		$floats = array('min_fov', 'max_fov');
+		$booleans = array('navbar', 'xmp');
+
+		foreach ($atts as $att => $value) {
+			// Unnamed attribute
+			if (is_int($att)) {
+				// ID
+				if (is_numeric($value) && !isset($atts['id']))
+					$atts['id'] = $value;
+
+				// Boolean
+				else
+					$atts[$value] = 1;
+
+				unset($atts[$att]);
+			}
+
+			// URL
+			else if ($att == 'url' && !preg_match('#^https?://#', $value))
+				$atts['url'] = 'http://' . $value;
+
+			// Size
+			else if (in_array($att, $sizes))
+				$atts[$att] = wpps_sanitize_size($value);
+
+			// Numbers
+			else if (in_array($att, $numbers))
+				$atts[$att] = intval($value);
+
+			// Floating-point numbers
+			else if (in_array($att, $floats))
+				$atts[$att] = floatval($value);
+
+			// Manual booleans
+			else if (in_array($att, $booleans))
+				$atts[$att] = intval(($value == 'yes'));
+		}
+	}
+
+	return $atts;
+}
 
 function wpps_handle_shortcode($atts) {
-	wpps_enqueue_scripts();
+	wp_enqueue_script('wp-photo-sphere');
 	$settings = get_option('wpps_settings');
 
-	$id = $atts[0];
-	$url = wp_get_attachment_url($id);
-	$text = str_replace('%title%', get_the_title($id), $settings['text']);
-	$width = (array_key_exists('width', $atts)) ? wpps_sanitize_size($atts['width']) : $settings['width'];
-	$max_width = (array_key_exists('max_width', $atts)) ? wpps_sanitize_size($atts['max_width']) : $settings['max_width'];
-	$height = (array_key_exists('height', $atts)) ? wpps_sanitize_size($atts['height'], array('px')) : $settings['height'];
-	$autoload = intval(in_array('autoload', $atts));
-	$anim_after = (array_key_exists('anim_after', $atts)) ? intval($atts['anim_after']) : 'default';
-	$style = $settings['style'] . ' width: ' . $width . '; max-width: ' . $max_width . ';';
+	// Attributes
+	$atts = wpps_shortcode_attributes($atts);
+	$atts = shortcode_atts(array(
+			'id' => 0,
+			'url' => '',
+			'title' => '',
+			'width' => $settings['width'],
+			'max_width' => $settings['max_width'],
+			'height' => intval($settings['height']),
+			'autoload' => 0,
+			'anim_after' => 'default',
+			'anim_speed' => $settings['anim_speed'],
+			'navbar' => $settings['navbar'],
+			'min_fov' => $settings['min_fov'],
+			'max_fov' => $settings['max_fov'],
+			'xmp' => $settings['xmp']
+		), $atts);
+
+	// URL and title
+	$title = (!empty($atts['title'])) ? $atts['title'] : $settings['text'];
+
+	if ($atts['id'] != 0) {
+		$id = $atts['id'];
+		$url = wp_get_attachment_url($id);
+		$text = str_replace('%title%', get_the_title($id), $title);
+	}
+
+	else {
+		$url = $atts['url'];
+		$text = str_replace('%title%', '', $title);
+	}
+
+	// Style
+	$style = $settings['style'] . ' width: ' . $atts['width'] . '; max-width: ' . $atts['max_width'] . ';';
 	$class_a = (!empty($settings['class_a'])) ? ' class="' . $settings['class_a'] . '"' : '';
 
+	// Display
 	$output = '<div class="wpps_container" style="' . $style . '">';
-	$output .= '<a href="' . $url . '?height=' . intval($height) . '&amp;hide=' . $settings['hide_link'] . '&amp;autoload=' . $autoload . '&amp;anim_after=' . $anim_after . '" style="display: block; ' . $settings['style_a'] . '"' . $class_a . '>' . $text . '</a>';
-	$output .= '<div style="position: relative;"></div>';
+
+	$params = implode('&amp;', array(
+			'height=' . $atts['height'],
+			'hide_link=' . $settings['hide_link'],
+			'autoload=' . $atts['autoload'],
+			'anim_after=' . $atts['anim_after'],
+			'anim_speed=' . $atts['anim_speed'],
+			'navbar=' . $atts['navbar'],
+			'min_fov=' . $atts['min_fov'],
+			'max_fov=' . $atts['max_fov'],
+			'xmp=' . $atts['xmp']
+		));
+
+	$output .= '<a href="' . $url . '?' . $params . '" style="display: block; ' . $settings['style_a'] . '"' . $class_a . '>' . $text . '</a>';
+
+	$output .= '<div style="position: relative; box-sizing: content-box;"></div>';
 	$output .= '</div>';
 
 	return $output;
 }
+add_shortcode('sphere', 'wpps_handle_shortcode');
 
 function wpps_create_menu() {
 	add_options_page('WP Photo Sphere', 'WP Photo Sphere', 'manage_options', __FILE__, 'wpps_options_page');
 	add_action('admin_init', 'wpps_register_settings');
 }
+add_action('admin_menu', 'wpps_create_menu');
 
 function wpps_register_settings() {
 	register_setting('wpps_options', 'wpps_settings', 'wpps_sanitize_settings');
@@ -110,6 +220,9 @@ function wpps_options_page() {
 			<?php
 			settings_fields('wpps_options');
 			$settings = get_option('wpps_settings');
+
+			$anim_speed = array();
+			preg_match('#^([0-9-]+(?:\.[0-9]*)?)([a-z ]+)$#', $settings['anim_speed'], $anim_speed);
 			?>
 			<table class="form-table">
 				<tr valign="top">
@@ -141,13 +254,47 @@ function wpps_options_page() {
 					<td><input type="text" id="wpps_settings_max_width" name="wpps_settings[max_width]" size="5" value="<?php echo $settings['max_width']; ?>" /></td>
 				</tr>
 				<tr valign="top">
+					<th><label for="wpps_settings_anim_speed"><?php _e('Default animation speed', 'wp-photo-sphere'); ?></label></th>
+					<td>
+						<input type="text" id="wpp_settings_anim_speed" name="wpps_settings[anim_speed_value]" size="4" value="<?php echo $anim_speed[1]; ?>" />
+						<select name="wpps_settings[anim_speed_unit]">
+							<option value="rpm" <?php selected($anim_speed[2], 'rpm'); ?>><?php _e('Revolutions per minute', 'wp-photo-sphere'); ?></option>
+							<option value="rps" <?php selected($anim_speed[2], 'rps'); ?>><?php _e('Revolutions per second', 'wp-photo-sphere'); ?></option>
+							<option value="dpm" <?php selected($anim_speed[2], 'dpm'); ?>><?php _e('Degrees per minute', 'wp-photo-sphere'); ?></option>
+							<option value="dps" <?php selected($anim_speed[2], 'dps'); ?>><?php _e('Degrees per second', 'wp-photo-sphere'); ?></option>
+							<option value="rad per minute" <?php selected($anim_speed[2], 'rad per minute'); ?>><?php _e('Radians per minute', 'wp-photo-sphere'); ?></option>
+							<option value="rad per second" <?php selected($anim_speed[2], 'rad per second'); ?>><?php _e('Radians per second', 'wp-photo-sphere'); ?></option>
+						</select>
+					</td>
+				</tr>
+				<tr valign="top">
 					<th><label for="wpps_settings_height"><?php _e('Default height', 'wp-photo-sphere'); ?></label></th>
 					<td><input type="text" id="wpps_settings_height" name="wpps_settings[height]" size="5" value="<?php echo $settings['height']; ?>" /></td>
 				</tr>
 
 				<tr valign="top">
 					<th><label for="wpps_settings_hide_link"><?php _e('Hide link', 'wp-photo-sphere'); ?></label></th>
-					<td><input type="checkbox" id="wpps_settings_hide_link" name="wpps_settings[hide_link]" value="1"<?php checked((1 == $settings['hide_link'])); ?> /></td>
+					<td><input type="checkbox" id="wpps_settings_hide_link" name="wpps_settings[hide_link]" value="1" <?php checked($settings['hide_link'], 1); ?> /></td>
+				</tr>
+
+				<tr valign="top">
+					<th><label for="wpps_settings_navbar"><?php _e('Display the navigation bar', 'wp-photo-sphere'); ?></label></th>
+					<td><input type="checkbox" id="wpps_settings_navbar" name="wpps_settings[navbar]" value="1" <?php checked($settings['navbar'], 1); ?> /></td>
+				</tr>
+
+				<tr valign="top">
+					<th><label for="wpps_settings_min_fov"><?php _e('Minimal field of view (in degrees)', 'wp-photo-sphere'); ?></label></th>
+					<td><input type="text" id="wpps_settings_min_fov" name="wpps_settings[min_fov]" value="<?php echo $settings['min_fov']; ?>" /></td>
+				</tr>
+
+				<tr valign="top">
+					<th><label for="wpps_settings_max_fov"><?php _e('Maximal field of view (in degrees)', 'wp-photo-sphere'); ?></label></th>
+					<td><input type="text" id="wpps_settings_max_fov" name="wpps_settings[max_fov]" value="<?php echo $settings['max_fov']; ?>" /></td>
+				</tr>
+
+				<tr valign="top">
+					<th><label for="wpps_settings_xmp"><?php _e('Read XMP data', 'wp-photo-sphere'); ?></label></th>
+					<td><input type="checkbox" id="wpps_settings_xmp" name="wpps_settings[xmp]" value="1" <?php checked($settings['xmp'], 1); ?> /></td>
 				</tr>
 			</table>
 			<?php submit_button(); ?>
@@ -162,7 +309,7 @@ function wpps_style_for_textarea($style) {
 }
 
 function wpps_sanitize_style($style) {
-	// Removing line breaks
+	// Removes line breaks
 	return trim(preg_replace('#;\s*#', '; ', str_replace('"', '\'', $style)));
 }
 
@@ -183,15 +330,21 @@ function wpps_sanitize_settings($values) {
 	$values['width'] = wpps_sanitize_size($values['width']);
 	$values['max_width'] = wpps_sanitize_size($values['max_width']);
 	$values['height'] = wpps_sanitize_size($values['height'], array('px'));
-	$values['hide_link'] = ($values['hide_link']) ? 1 : 0;
+	$values['hide_link'] = (!!$values['hide_link']) ? 1 : 0;
+	$values['navbar'] = (!!$values['navbar']) ? 1 : 0;
+	$values['min_fov'] = floatval($values['min_fov']);
+	$values['max_fov'] = floatval($values['max_fov']);
+	$values['xmp'] = (!!$values['xmp']) ? 1 : 0;
+
+	// Animation speed
+	$speed_unit = $values['anim_speed_unit'];
+	if (!in_array($speed_unit, array('rpm', 'rps', 'dpm', 'dps', 'rad per minute', 'rad per second')))
+		$speed_unit = 'rpm';
+
+	$values['anim_speed'] = floatval($values['anim_speed_value']) . $speed_unit;
+
+	unset($values['anim_speed_value'], $values['anim_speed_unit']);
+
 	return $values;
 }
-
-register_activation_hook(__FILE__, 'wpps_activation');
-register_deactivation_hook(__FILE__, 'wpps_deactivation');
-add_shortcode('sphere', 'wpps_handle_shortcode');
-add_action('admin_menu', 'wpps_create_menu');
-add_action('wp_enqueue_media', 'wpps_enqueue_admin_scripts');
-add_action('media_buttons', 'wpps_add_pano_button', 15);
-add_action('plugins_loaded', 'wpps_lang');
 ?>
